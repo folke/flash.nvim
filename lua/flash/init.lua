70130d29a3c4c8d90d96caae5871d0cc19e3f283
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-06-08 10:47:16 +0200

    feat: expose commands on main module

--- lua/flash/init.lua
+++ lua/flash/init.lua
@@ -1,7 +1,12 @@
+---@type Flash.Commands
 local M = {}
 
 function M.setup()
   require("flash.config").setup()
 end
 
-return M
+return setmetatable(M, {
+  __index = function(_, k)
+    return require("flash.commands")[k]
+  end,
+})
