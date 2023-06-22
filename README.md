a8960a40e7c414cfaa9ca228e55fe7494a0518bf
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-06-22 08:40:03 +0200

    docs: added more advanced example for diagnostics

--- README.md
+++ README.md
@@ -249,6 +249,26 @@ require("flash").jump({
     end)
   end,
 })
+
+-- More advanced example that also highlights diagnostics:
+require("flash").jump({
+  matcher = function(win)
+    ---@param diag Diagnostic
+    return vim.tbl_map(function(diag)
+      return {
+        pos = { diag.lnum + 1, diag.col },
+        end_pos = { diag.end_lnum + 1, diag.end_col - 1 },
+      }
+    end, vim.diagnostic.get(vim.api.nvim_win_get_buf(win)))
+  end,
+  action = function(match, state)
+    vim.api.nvim_win_call(match.win, function()
+      vim.api.nvim_win_set_cursor(match.win, match.pos)
+      vim.diagnostic.open_float()
+      vim.api.nvim_win_set_cursor(match.win, state.pos)
+    end)
+  end,
+})
 ```
 
 </details>
--- lua/flash/plugins/diagnostics.lua
+++ lua/flash/plugins/diagnostics.lua
@@ -3,17 +3,14 @@ local M = {}
 -- Example plugin that shows labels at positions with diagnostics.
 function M.show()
   require("flash").jump({
-    search = { multi_window = true, wrap = true },
-    highlight = { backdrop = true, label = { current = true } },
     matcher = function(win)
-      local buf = vim.api.nvim_win_get_buf(win)
       ---@param diag Diagnostic
       return vim.tbl_map(function(diag)
         return {
           pos = { diag.lnum + 1, diag.col },
           end_pos = { diag.end_lnum + 1, diag.end_col - 1 },
         }
-      end, vim.diagnostic.get(buf))
+      end, vim.diagnostic.get(vim.api.nvim_win_get_buf(win)))
     end,
     action = function(match, state)
       vim.api.nvim_win_call(match.win, function()
