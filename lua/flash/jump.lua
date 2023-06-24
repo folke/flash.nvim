16f785f26e74b8f0b49901356c57cda2a06379f5
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-06-25 00:31:41 +0200

    fix(jump): improved operator pending mode for jumps

--- lua/flash/jump.lua
+++ lua/flash/jump.lua
@@ -1,19 +1,10 @@
+local Pos = require("flash.search.pos")
 local M = {}
 
 ---@param match Flash.Match
 ---@param state Flash.State
 ---@return Flash.Match?
 function M.jump(match, state)
-  -- fix inclusive/exclusive
-  -- default is exclusive
-  local inclusive = state.opts.jump.inclusive
-  if inclusive == nil then
-    inclusive = vim.fn.mode(true):sub(1, 2) == "no" and state.opts.jump.pos ~= "range"
-  end
-  if inclusive then
-    vim.cmd("normal! v")
-  end
-
   -- add to jump list
   if state.opts.jump.jumplist then
     vim.cmd("normal! m'")
@@ -24,22 +15,39 @@ function M.jump(match, state)
     vim.api.nvim_set_current_win(match.win)
   end
 
-  -- jump to start
-  if state.opts.jump.pos == "start" then
-    vim.api.nvim_win_set_cursor(match.win, match.pos)
-
-  -- jump to end
-  elseif state.opts.jump.pos == "end" then
-    vim.api.nvim_win_set_cursor(match.win, match.end_pos)
-
   -- select range
-  else
+  if state.opts.jump.pos == "range" then
     if vim.fn.mode() == "v" then
       vim.cmd("normal! v")
     end
     vim.api.nvim_win_set_cursor(match.win, match.pos)
     vim.cmd("normal! v")
     vim.api.nvim_win_set_cursor(match.win, match.end_pos)
+  else
+    local mode = vim.fn.mode(true)
+
+    local pos = state.opts.jump.pos == "start" and match.pos or match.end_pos
+
+    if mode:sub(1, 2) == "no" then
+      -- fix inclusive/exclusive
+      -- default is exclusive
+      if state.opts.jump.inclusive ~= false then
+        vim.cmd("normal! v")
+      end
+
+      local current = Pos(vim.api.nvim_win_get_cursor(match.win))
+      local offset = state.opts.jump.offset
+
+      if not offset and state.opts.jump.pos == "end" and pos < current then
+        offset = 1
+      end
+
+      pos = pos + Pos({ 0, offset or 0 })
+      ---@cast pos Pos
+      pos[2] = math.max(0, pos[2])
+    end
+
+    vim.api.nvim_win_set_cursor(match.win, pos)
   end
 end
 
