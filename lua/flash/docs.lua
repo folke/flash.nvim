f5e6a8d946bc6b6a80c894333502d0fae8763dd0
Carlos Calla <calla.alarcon@gmail.com>
2025-05-02 22:43:45 +0200

    Update quotes

--- lua/flash/docs.lua
+++ lua/flash/docs.lua
@@ -20,7 +20,7 @@ function M.suggested()
     -- stylua: ignore
     keys = {
       { "s", mode = { "n", "x" }, function() require("flash").jump() end, desc = "Flash" },
-      { 's', mode = { 'o' }, function() require('flash').jump { search = { multi_window = false } } end, desc = 'Flash' },
+      { "s", mode = { "o" }, function() require("flash").jump { search = { multi_window = false } } end, desc = "Flash" },
       { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
       { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
       { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
