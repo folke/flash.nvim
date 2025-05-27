9bb6b5d957a914586a1c7ee5099e6ec55fc19502
Kaiser-Yang <624626089@qq.com>
2025-05-27 15:20:20 +0800

    fix(jump): ignore blink-cmp-menu by default

--- lua/flash/config.lua
+++ lua/flash/config.lua
@@ -36,6 +36,7 @@ local defaults = {
       "cmp_menu",
       "noice",
       "flash_prompt",
+      "blink-cmp-menu",
       function(win)
         -- exclude non-focusable windows
         return not vim.api.nvim_win_get_config(win).focusable
